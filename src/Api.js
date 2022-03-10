fb917c881a9b8127cbf5cad08b102e50d6ef444f
username <monika.teslak@gmail.com>
2022-03-10 21:44:49 +0100

    add data api component

---
+++ src/Api.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const DataApi = () => {
+    return (
+        <div>
+            <main className='data-api'></main>
+        </div>
+    )
+}
--- src/components/AllPostList.js
+++ src/components/AllPostList.js
@@ -1,17 +1,23 @@
-import React from "react";
-import { ReactDOM } from "react-dom";
+import React from 'react'
+import { ReactDOM } from 'react-dom'
+import { DataApi } from '../Api'
 
 function AllPostList() {
-    return <div className="wraper">
-        <ul>
+  return (
+    <div className="wraper">
+      <section>
+        <DataApi></DataApi>
+      </section>
+      <ul>
         <li>tu będą posty</li>
         <li> pobierz wszystkie posty do tablicy</li>
         <li>i przejdz po nich wyswietlając wszystko</li>
         <li>tu będą posty</li>
         <li> pobierz wszystkie posty do tablicy</li>
         <li>i przejdz po nich wyswietlając wszystko</li>
-        </ul>
+      </ul>
     </div>
+  )
 }
 
 export default AllPostList
