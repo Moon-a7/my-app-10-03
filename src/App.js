31244cffc32b4846634cea502affd74f41e657f5
username <monika.teslak@gmail.com>
2022-03-10 20:55:31 +0100

    step-one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ReactDOM } from 'react-dom'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="views">
+          <div className="all-post">all post list</div>
+          <div className="post-comments">post + comments</div>
+          <div className="form">form add new comments</div>
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
